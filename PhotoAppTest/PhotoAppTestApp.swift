83510b6afbd6bf9a82bf8d28454596abb788935b
Sbat2000 <zevzok@gmail.com>
2024-08-16 21:16:08 +0800

    Initial Commit

---
+++ PhotoAppTest/PhotoAppTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoAppTestApp.swift
+//  PhotoAppTest
+//
+//  Created by Aleksandr Garipov on 16.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct PhotoAppTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
