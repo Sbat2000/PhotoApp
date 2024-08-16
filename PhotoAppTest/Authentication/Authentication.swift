72e3efad7d59d3718e95f97f7a574a97ae10ab2e
Sbat2000 <zevzok@gmail.com>
2024-08-16 21:34:53 +0800

    [PAT-FB] добавил firebase

---
+++ PhotoAppTest/Authentication/Authentication.swift
@@ -0,0 +1,18 @@
+//
+//  Authentication.swift
+//  PhotoAppTest
+//
+//  Created by Aleksandr Garipov on 16.08.2024.
+//
+
+import SwiftUI
+
+struct Authentication: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Authentication()
+}
---
+++ PhotoAppTest/Home/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeView.swift
+//  PhotoAppTest
+//
+//  Created by Aleksandr Garipov on 16.08.2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Text("userID: ")
+            Text("user email: ")
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
