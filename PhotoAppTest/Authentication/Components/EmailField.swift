51abbe5be657440fd8a3f5d46f951e871d0744da
Sbat2000 <zevzok@gmail.com>
2024-08-17 00:18:14 +0800

    [PAT-AF] добавил показ ошибки по email

--- PhotoAppTest/Authentication/Components/EmailField.swift
+++ PhotoAppTest/Authentication/Components/EmailField.swift
@@ -12,15 +12,23 @@ struct EmailField: View {
     @ObservedObject var email: Email
 
     var body: some View {
-        TextField(LocalizedStrings.email, text: $email.value)
-            .textInputAutocapitalization(.never)
-            .padding()
-            .overlay(RoundedRectangle(cornerRadius: 8)
-                .stroke(email.isValid ? .gray.opacity(0.2) : .red, lineWidth: 2))
-            .padding(.horizontal, 16)
-            .onSubmit {
-                email.submitted()
-            }
+        VStack{
+            TextField(LocalizedStrings.email, text: $email.value)
+                .textInputAutocapitalization(.never)
+                .padding()
+                .overlay(RoundedRectangle(cornerRadius: 8)
+                    .stroke(email.isValid ? .gray.opacity(0.2) : .red, lineWidth: 2))
+                .padding(.horizontal, 16)
+                .onSubmit {
+                    email.submitted()
+                }
+
+            Text(email.error)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .fontWeight(.medium)
+                .foregroundStyle(Color.red)
+                .padding(.horizontal, 16)
+        }
     }
 }
 
